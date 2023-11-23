5df0bd40a4d3404aff200e0bc7702126e7fdc79b
rbska <rbska@DESKTOP-QD2V6EG>
2023-11-23 19:36:10 +0900

    커밋 & 푸쉬 테스트

---
+++ javahome/src/variable/EX2_1.java
@@ -0,0 +1,10 @@
+package variable;
+
+public class EX2_1 {
+
+	public static void main(String[] args) {
+		System.out.println("hellow, world");
+		System.out.print("world");
+		System.out.println("hellow");
+	}
+}
